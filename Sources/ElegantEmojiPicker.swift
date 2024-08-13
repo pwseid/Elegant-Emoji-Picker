f1940de647247d67a532caeca0b29aa8154d8d09
pwseid <pwseid@users.noreply.github.com>
2024-08-13 14:37:42 -0700

    Update ElegantEmojiPicker.swift

--- Sources/ElegantEmojiPicker.swift
+++ Sources/ElegantEmojiPicker.swift
@@ -193,7 +193,7 @@ open class ElegantEmojiPicker: UIViewController {
     
     public override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        collectionLayout.headerReferenceSize = CGSize(width: collectionView.frame.width, height: 50)
+        collectionLayout.headerReferenceSize = CGSize(width: collectionView.frame.width, height: 0)
         fadeContainer.layer.mask?.frame = fadeContainer.bounds
     }
     
